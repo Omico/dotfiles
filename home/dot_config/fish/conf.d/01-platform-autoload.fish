cd507e109e057bf4fc3e22a889d91f52d036c826
Omico <me@omico.icu>
2026-03-16 02:14:22 +0800

    refactor(fish): Use fish_platform and platform-aware chezmoi add
    - Use $fish_platform directly in 01-platform-autoload.fish
    - In chezmoi.fish use __fish_config_dir and add platform-specific dirs
      only on linux/darwin/wsl; use $HOME instead of ~

--- home/dot_config/fish/conf.d/01-platform-autoload.fish
+++ home/dot_config/fish/conf.d/01-platform-autoload.fish
@@ -1,11 +1,9 @@
 #!/usr/bin/env fish
 
-set -l __platform $fish_platform
+__fish_load_config_dir "$__fish_config_dir/conf.d/$fish_platform"
+__fish_load_config_dir "$__fish_config_dir/functions/$fish_platform"
 
-__fish_load_config_dir "$__fish_config_dir/conf.d/$__platform"
-__fish_load_config_dir "$__fish_config_dir/functions/$__platform"
-
-if contains "$__platform" linux darwin wsl
+if contains "$fish_platform" linux darwin wsl
     __fish_load_config_dir "$__fish_config_dir/conf.d/unix"
     __fish_load_config_dir "$__fish_config_dir/functions/unix"
 end
--- home/dot_config/fish/functions/unix/chezmoi.fish
+++ home/dot_config/fish/functions/unix/chezmoi.fish
@@ -1,19 +1,24 @@
 #!/usr/bin/env fish
 
 function chezmoi_add_configs
-    set fish_config ~/.config/fish
+    set -l __fish_config_dir $HOME/.config/fish
 
-    fish_indent -w $fish_config/**/*.fish
+    fish_indent -w $__fish_config_dir/**/*.fish
 
-    find $fish_config -type d -exec chmod 755 {} \;
-    find $fish_config -type f -exec chmod 644 {} \;
+    find $__fish_config_dir -type d -exec chmod 755 {} \;
+    find $__fish_config_dir -type f -exec chmod 644 {} \;
 
-    chezmoi forget --force $fish_config/conf.d/
-    chezmoi forget --force $fish_config/functions/
+    chezmoi forget --force $__fish_config_dir/conf.d/$fish_platform
+    chezmoi forget --force $__fish_config_dir/functions/$fish_platform
 
-    chezmoi add $fish_config/conf.d/**/*.fish
-    chezmoi add $fish_config/functions/**/*.fish
+    chezmoi add $__fish_config_dir/conf.d/*.fish
+    chezmoi add $__fish_config_dir/functions/*.fish
 
-    chezmoi forget --force ~/.config/starship.toml
-    chezmoi add ~/.config/starship.toml
+    if contains "$fish_platform" linux darwin wsl
+        chezmoi add $__fish_config_dir/conf.d/$fish_platform/*.fish
+        chezmoi add $__fish_config_dir/functions/$fish_platform/*.fish
+    end
+
+    chezmoi forget --force $HOME/.config/starship.toml
+    chezmoi add $HOME/.config/starship.toml
 end
