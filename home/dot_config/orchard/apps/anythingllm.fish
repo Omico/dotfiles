ec47903e1ba88436ec2d0a4826fea4f5a94116e6
Omico <me@omico.icu>
2026-04-11 04:24:20 +0800

    chore(orchard): Remove AnythingLLM app definition

--- home/dot_config/orchard/apps/anythingllm.fish
+++
@@ -1,4 +0,0 @@
-set -g orchard_app_id anythingllm
-set -g orchard_app_display_name AnythingLLM
-set -g orchard_app_download_url "https://cdn.anythingllm.com/latest/AnythingLLMDesktop-Silicon.dmg"
-set -g orchard_app_download_type dmg
