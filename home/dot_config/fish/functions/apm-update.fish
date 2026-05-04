ebdee114c8661d03443f07a6c326add2a33b2b70
Omico <me@omico.icu>
2026-05-04 08:37:45 +0800

    chore(apm): Update skills dependency management

--- home/dot_config/fish/functions/apm-update.fish
+++ home/dot_config/fish/functions/apm-update.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
-function apm-update --description "Update agent skills via APM (apm install --update in ~/.apm)"
+function apm-update --description "Update agent skills via APM (apm deps update in ~/.apm)"
     echo "Updating agent skills from ~/.apm..."
 
     __apm-run-global-apm update; or return $status
-    __apm-run-global-apm install --update $argv; or return $status
+    __apm-run-global-apm deps update --parallel-downloads 32 $argv; or return $status
 
     __link-apm-skills-to-agents
 end
