c972b907d71c07a53ecdd51717f9a7c1c5ffabe2
Omico <me@omico.icu>
2026-05-23 23:36:40 +0800

    refactor(fish): Extract fnm LTS helpers in upgrade function
    Centralize remote and installed version parsing, retry install once when
    LTS is missing, and reuse the installed list for uninstall.

--- home/dot_config/fish/functions/fnm-upgrade-latest-lts.fish
+++ home/dot_config/fish/functions/fnm-upgrade-latest-lts.fish
@@ -6,12 +6,17 @@ function fnm-upgrade-latest-lts --description "Install LTS, set default, uninsta
         return 1
     end
 
-    set -l latest_lts (fnm list-remote --lts --latest 2>/dev/null | awk '{print $1}')
-    set -l installed (fnm list 2>/dev/null | awk '{print $2}' | string match -r '^v')
+    set -l installed (__fnm_installed_node_versions)
+    set -l latest_lts
 
-    if test -z "$latest_lts"; or not contains "$latest_lts" $installed
+    for _attempt in 1 2
+        set latest_lts (__fnm_remote_latest_lts)
+        if test -n "$latest_lts"; and contains "$latest_lts" $installed
+            break
+        end
+        test $_attempt -eq 1; or break
         fnm install --lts; or return 1
-        set latest_lts (fnm list-remote --lts --latest 2>/dev/null | awk '{print $1}')
+        set installed (__fnm_installed_node_versions)
     end
 
     if test -z "$latest_lts"
@@ -22,9 +27,9 @@ function fnm-upgrade-latest-lts --description "Install LTS, set default, uninsta
     fnm use "$latest_lts"; or return 1
     fnm default "$latest_lts"; or return 1
 
-    for v in (fnm list 2>/dev/null | awk '{print $2}' | string match -r '^v')
+    for v in $installed
         if test "$v" != "$latest_lts"
-            fnm uninstall $v; or printf "fnm: failed to uninstall %s\n" $v >&2
+            fnm uninstall "$v"; or printf "fnm: failed to uninstall %s\n" $v >&2
         end
     end
 
@@ -35,4 +40,12 @@ function fnm-upgrade-latest-lts --description "Install LTS, set default, uninsta
             corepack install -g pnpm@latest
         end
     end
+end
+
+function __fnm_installed_node_versions --description 'internal: installed Node semver list from fnm list'
+    fnm list 2>/dev/null | awk '{print $2}' | string match -r '^v[0-9]+\.[0-9]+\.[0-9]+'
+end
+
+function __fnm_remote_latest_lts --description 'internal: latest LTS semver from fnm list-remote'
+    string split -f1 ' ' -- (fnm list-remote --lts --latest 2>/dev/null)
 end
